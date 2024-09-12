32313c0f4e69420d3d3ab3ec6e527bbd82de82c6
Anirudh Konidala <68204671+kidskoding@users.noreply.github.com>
2024-09-12 16:33:47 -0500

    added game over screen

--- src/main.py
+++ src/main.py
@@ -6,7 +6,9 @@ pygame.init()
 
 grid = Grid()
 
-screen = pygame.display.set_mode((440, 440))
+dim = 440
+
+screen = pygame.display.set_mode((dim, dim))
 pygame.display.set_caption('2048')
 screen.fill((187, 173, 160))
 
@@ -30,16 +32,32 @@ while playing:
     grid.drawGrid(screen, tileSize=100, tileMargin=10)
     
     if isGameOver():
-        overlay = pygame.Surface((440, 440))
-        overlay.set_alpha(128)
+        overlay = pygame.Surface((dim, dim))
+        overlay.set_alpha(150)
         overlay.fill((187, 173, 160))
         screen.blit(overlay, (0, 0))
         
         font = pygame.font.Font(None, 74)
-        text = font.render('Game Over', True, (119, 110, 101))
-        text_rect = text.get_rect(center=(220, 220))
+        text = font.render('Game Over!', True, (119, 110, 101))
+        text_rect = text.get_rect(center=(dim / 2, dim / 2 - 70))
         screen.blit(text, text_rect)
-    
+        
+        rect_width = 200
+        rect_height = 50
+        rect_font = pygame.font.Font(None, 36)
+        
+        play_again_rect = pygame.Rect(dim / 2 - rect_width / 2, dim / 2 - 20, rect_width, rect_height)
+        text = rect_font.render('Play Again', True, (255, 255, 255))
+        text_rect = text.get_rect(center=play_again_rect.center)
+        pygame.draw.rect(screen, (143, 122, 102), play_again_rect)
+        screen.blit(text, text_rect)
+        
+        quit_rect = pygame.Rect(dim / 2 - rect_width / 2, dim / 2 + 50, rect_width, rect_height)
+        text = rect_font.render('Quit', True, (255, 255, 255))
+        text_rect = text.get_rect(center=quit_rect.center)
+        pygame.draw.rect(screen, (143, 122, 102), quit_rect)
+        screen.blit(text, text_rect)
+        
     pygame.display.update()
 
 pygame.quit()
